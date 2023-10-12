2dd14480e0a24e9d7d1054dae09d63970d518987
edhungerford <79060679+edhungerford@users.noreply.github.com>
2023-10-12 00:31:47 -0400

    Cleaning up local routes.

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ app.use(cors());
 
 app.get('/api', (req, res) => {
     res.json({
-        gradia: "http://localhost:3001/api/gradia",
-        bsbs: "http://localhost:3001/api/bsbs"
+        gradia: "https://gradia.edsite.black/api/gradia",
+        bsbs: "https://gradia.edsite.black/api/bsbs"
     })
 })
 
 //TAILS OF GRADIA
 app.get('/api/gradia', (req, res) => {
     res.json({
-        characters: 'http://localhost:3001/api/gradia/characters',
-        story: "http://localhost:3001/api/gradia/story"});
+        characters: 'https://gradia.edsite.black/api/gradia/characters',
+        story: "https://gradia.edsite.black/api/gradia/story"});
 });
 
 app.use('/api/gradia/characters', charRouter);
@@ -27,8 +27,8 @@ app.use('/api/gradia/story', storyRouter);
 //BLUE SKIES, BLACK SMOKE
 app.get('/api/bsbs', (req, res) => {
     res.json({
-        characters: 'http://localhost:3001/api/bsbs/characters',
-        story: "http://localhost:3001/api/bsbs/story"});
+        characters: 'https://gradia.edsite.black/api/bsbs/characters',
+        story: "https://gradia.edsite.black/api/bsbs/story"});
 });
 
 app.use('/api/bsbs/characters', charRouter);
